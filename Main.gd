117654f6ed3826128a0d26485ee63596b71e317b
Julian Todd <henrylmeyer2013@gmail.com>
2020-12-17 13:32:12 +0000

    crude animation added

--- Main.gd
+++ Main.gd
@@ -5,7 +5,11 @@ var info_text = """Hello there
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#vr.initialize()
+	vr.initialize()
 	$OQ_UILabel.set_label_text(info_text);
 	pass
 
+
+
+func glidergo():
+	pass # Replace with function body.
