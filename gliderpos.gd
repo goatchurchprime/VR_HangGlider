3fea9af8b6f5e1144fa9dd5f856fbb0400e5f5c0
DangerousDarren <darrenbrown50@hotmail.com>
2020-12-31 17:44:00 +0000

    Added sound, changes pitch & volume. 3D aspect not working but unsure why. Should just be a case of removing the listener, so will directionally listen from the camera position. Doing this works with the PC, but sound goes off on the Quest

--- gliderpos.gd
+++ gliderpos.gd
@@ -6,6 +6,7 @@ onready var arvrorigin = get_node("../OQ_ARVROrigin")
 onready var headcam = get_node("../OQ_ARVROrigin/OQ_ARVRCamera")
 onready var label = get_node("../OQ_UILabel")
 onready var orgpos = transform.origin
+onready var windNoise = get_node("glider2/WindNoise3D")
 
 var gliderkinematics = null
 var gliderdynamicstate = null
@@ -14,6 +15,7 @@ func _ready():
 	gliderkinematics = load("res://gliderkinematics.gd").new($AeroCentre)
 	gliderdynamicstate = gliderkinematics.initgliderstate()
 	gliderdynamicstate.stepflight(self, 0.0)
+	windNoise.play()
 		
 var recarvrorigin = null
 var headcamoffset = Vector3(0,0,0)
@@ -38,6 +40,16 @@ func _physics_process(delta):
 
 	gliderkinematics.flightforcesstate(gliderdynamicstate, Lb, self)
 	gliderdynamicstate.stepflight(self, delta)
+
+		
+	#Link wind noise volume and pitch to glider velocity
+	#lb range = -0.6 (push out) to 0.2 (pull in)
+
+	var windVolume =   gliderdynamicstate.v * 5 - 30 # 2.5 - 40
+	windNoise.unit_db = windVolume 
+	var windPitch = gliderdynamicstate.v * 0.08 -.1#-.4
+	windNoise.pitch_scale = windPitch
+	label.set_label_text("Lb=%f\nvolume=%f" % [Lb, rad2deg(windVolume)])
 	
 	if recarvrorigin == null:
 		if abs(transform.origin.x - orgpos.x) > abs(orgpos.x)*2:
@@ -45,7 +57,7 @@ func _physics_process(delta):
 	elif abs(transform.origin.x - orgpos.x) > abs(orgpos.x)*20:
 		transform.origin = orgpos
 		
-	label.set_label_text("Lb=%f\npitch=%f" % [Lb, rad2deg(gliderdynamicstate.fr)])
+	#label.set_label_text("Lb=%f\npitch=%f" % [Lb, rad2deg(gliderdynamicstate.fr)])
 	
 	if pitchcontroller._button_just_pressed(vr.CONTROLLER_BUTTON.YB):
 		if recarvrorigin == null:
