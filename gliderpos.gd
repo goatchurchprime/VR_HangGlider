b736b440cee434394f4a1d1df9b0f91ec2ccf460
Julian Todd <julian@goatchurch.org.uk>
2021-01-02 15:04:31 +0000

    apply patch to undo whatever darren did to break it

--- gliderpos.gd
+++ gliderpos.gd
@@ -10,14 +10,6 @@ onready var windNoise = get_node("glider2/WindNoise3D")
 var gliderkinematics = null
 var gliderdynamicstate = null
 
-<<<<<<< HEAD
-#func _ready():
-#	gliderkinematics = load("res://gliderkinematics.gd").new($AeroCentre)
-#	gliderdynamicstate = gliderkinematics.initgliderstate()
-#	gliderdynamicstate.stepflight(self, 0.0)
-	#windNoise.play()
-		
-=======
 func takeoffstart():
 	mode = RigidBody.MODE_KINEMATIC
 	transform.origin = orgpos
@@ -29,61 +21,10 @@ func _ready():
 	gliderkinematics = load("res://gliderkinematics.gd").new($AeroCentre)
 	takeoffstart()
 	
->>>>>>> 66b7894d7835ee3942333729880a0c0f1519cad3
 var recarvrorigin = null
 var headcamoffset = Vector3(0,0,0)
 export var stationary = false
 
-<<<<<<< HEAD
-#func _physics_process(delta):
-	
-#	var camvec = -headcam.global_transform.basis.z
-#	if is_nan(camvec.x):
-#		print("nan value in ARVRCamera!")
-#		return
-#
-#	var controllerdisp = pitchcontroller.global_transform.origin - headcam.global_transform.origin
-#	var handdist = Vector3(camvec.x, 0, camvec.z).normalized().dot(controllerdisp)	
-#	if recarvrorigin != null:  # in flight hand disp needs to be in direction of flight or it responds to head turning
-#		var controlframeheading = $AeroCentre/TetherPoint/AframeBisector.global_transform.basis.z
-#		handdist = controlframeheading.dot(controllerdisp)
-#
-#	var h = $AeroCentre/TetherPoint/HangStrap.mesh.size.y/2
-#	var Lb = clamp(-handdist, -h*0.9, h*0.9)
-#	var epsilon = rad2deg(asin(Lb/h))
-#	$AeroCentre/TetherPoint/HangStrap.rotation_degrees.x = -epsilon + $AeroCentre/TetherPoint/AframeBisector.rotation_degrees.x
-#
-#	gliderkinematics.flightforcesstate(gliderdynamicstate, Lb, self)
-#	gliderdynamicstate.stepflight(self, delta)
-#
-#
-#	if recarvrorigin == null:
-#		if abs(transform.origin.x - orgpos.x) > abs(orgpos.x)*2:
-#			transform.origin = orgpos
-#	elif abs(transform.origin.x - orgpos.x) > abs(orgpos.x)*20:
-#		transform.origin = orgpos
-#
-#	#label.set_label_text("Lb=%f\npitch=%f" % [Lb, rad2deg(gliderdynamicstate.fr)])
-#
-#	if pitchcontroller._button_just_pressed(vr.CONTROLLER_BUTTON.YB):
-#		if recarvrorigin == null:
-#			recarvrorigin = arvrorigin.global_transform.origin
-#			headcamoffset = headcam.global_transform.origin - arvrorigin.global_transform.origin
-#			#arvrorigin.global_transform.origin = $AeroCentre/TetherPoint/HangStrap/PilotBody/PilotHead.global_transform.origin - headcamoffset
-#		else:
-#			arvrorigin.global_transform.origin = recarvrorigin
-#			recarvrorigin = null
-#	if recarvrorigin != null:
-#		arvrorigin.global_transform.origin = $AeroCentre/TetherPoint/HangStrap/PilotBody/PilotHead.global_transform.origin - headcamoffset
-#
-#	#Link wind noise volume and pitch to glider velocity
-#	#lb range = -0.6 (push out) to 0.2 (pull in)
-#	var windVolume =   gliderdynamicstate.v * 5 - 30 # 2.5 - 40
-#	windNoise.unit_db = windVolume 
-#	var windPitch = gliderdynamicstate.v * 0.08 -.1#-.4
-#	windNoise.pitch_scale = windPitch
-#	label.set_label_text("Lb=%f\nvolume=%f" % [Lb, rad2deg(windVolume)])
-=======
 func _process(delta):
 	if Input.is_action_just_pressed("ui_home") or pitchcontroller._button_just_pressed(vr.CONTROLLER_BUTTON.YB):
 		if recarvrorigin == null:
@@ -155,4 +96,3 @@ func _physics_process(delta):
 	
 	if recarvrorigin != null:
 		arvrorigin.global_transform.origin = $AeroCentre/TetherPoint/HangStrap/PilotBody/PilotHead.global_transform.origin - headcamoffset
->>>>>>> 66b7894d7835ee3942333729880a0c0f1519cad3
